62155ec2d93ce7edd25f99a4aef24223f1645c31
dulithahsith <129386645+dulithahsith@users.noreply.github.com>
2026-02-28 11:44:42 +0530

    Fix Google OAuth implementation

--- client/src/components/Auth/auth.js
+++ client/src/components/Auth/auth.js
@@ -9,9 +9,14 @@ import {
 } from "@material-ui/core";
 import useStyles from "../../styles";
 import Input from "./input";
+import { useGoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Icon from "./icon";
+import { useDispatch } from "react-redux";
+
 const Auth = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignUp] = useState(false);
 
@@ -20,6 +25,44 @@ const Auth = () => {
   const switchMode = () => setIsSignUp((currentMode) => !currentMode);
   const handleChange = () => {};
   const handleSubmit = () => {};
+  const googleSuccess = async (credentialResponse) => {
+    console.log(credentialResponse); // contains res.credential
+    const token = credentialResponse?.credential;
+    const res = await fetch(
+      "https:www.googleapis.com/oauth2/v3/userinfo?alt=json",
+      { headers: { Authorization: `Bearer ${token}` } },
+    );
+    const profile = await res.json();
+
+    try {
+      dispatch({ type: "AUTH", data: { profile: profile, token: token } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const googleFailure = (error) => {
+    console.log(error);
+    console.log("Google Login was not successfull");
+  };
+  const GoogleButton = () => {
+    const login = useGoogleLogin({
+      onSuccess: googleSuccess,
+      onError: googleFailure,
+    });
+
+    return (
+      <Button
+        className={classes.googleButton}
+        color="primary"
+        fullWidth
+        onClick={() => login()}
+        startIcon={<Icon />}
+        variant="contained"
+      >
+        Google Sign In
+      </Button>
+    );
+  };
   return (
     <Container component="main" maxWidth="xs">
       <Paper className="classes.paper" elevation={3}>
@@ -80,6 +123,9 @@ const Auth = () => {
           >
             <center> {isSignup ? "Sign Up" : "Sign In"}</center>
           </Button>
+          <GoogleOAuthProvider clientId="486481498459-48vt73gen4niuh90j2cftpiltjuatosb.apps.googleusercontent.com">
+            <GoogleButton />
+          </GoogleOAuthProvider>
           <Grid container justify="flex-end">
             <Grid item>
               <Button onClick={switchMode}>
